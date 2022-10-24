705158e69c28fcca6b5914e2739310f7f8581c5d
Bebetto Francis <bebettofrancis@gmail.com>
2022-10-24 23:41:53 +0530

    First commit

---
+++ src/main/java/com/bebetto/financemanager/FinancemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.bebetto.financemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancemanagerApplication.class, args);
+	}
+
+}
